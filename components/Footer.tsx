1697fb92f5e806b02bc2ff67fa5fc52a79d0f1ce
Aryan Prajapati <prajapatiaryan3103@gmail.com>
2025-02-28 12:52:18 +0530

    message

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import { MessageCircle, Instagram, Youtube, Newspaper } from "lucide-react";
-
+import {
+  IconBrandBlogger,
+  IconBrandGithub,
+  IconBrandYoutube,
+  IconBrandInstagram,
+} from "@tabler/icons-react";
 const Footer = () => {
   const navigationLinks = {
     Home: {
@@ -10,7 +15,6 @@ const Footer = () => {
       "Plans & Pricing": "/pricing",
     },
     Features: {
-      
       "Text to Image": "/text-to-image",
       "Text to Video (soon)": "#",
       "Sketch to Image (soon)": "#",
@@ -34,28 +38,32 @@ const Footer = () => {
 
   const socialLinks = [
     {
-      icon: MessageCircle,
+      title: "GitHub",
+      icon: IconBrandGithub,
       href: "#",
       hoverColor: "hover:text-blue-500",
       borderHoverColor: "hover:border-blue-500",
       glowColor: "hover:shadow-[0_0_15px_rgba(59,130,246,0.5)]",
     },
     {
-      icon: Instagram,
+      title: "Instagram",
+      icon: IconBrandInstagram,
       href: "#",
-      hoverColor: "hover:text-pink-500",
-      borderHoverColor: "hover:border-pink-500",
+      hoverColor: "hover:text-pink-800",
+      borderHoverColor: "hover:border-pink-800",
       glowColor: "hover:shadow-[0_0_15px_rgba(236,72,153,0.5)]",
     },
     {
-      icon: Youtube,
+      title: "Youtube",
+      icon: IconBrandYoutube,
       href: "#",
-      hoverColor: "hover:text-red-600",
-      borderHoverColor: "hover:border-red-600",
+      hoverColor: "hover:text-red-700",
+      borderHoverColor: "hover:border-red-700",
       glowColor: "hover:shadow-[0_0_15px_rgba(220,38,38,0.5)]",
     },
     {
-      icon: Newspaper,
+      title: "Blog",
+      icon: IconBrandBlogger,
       href: "#",
       hoverColor: "hover:text-green-500",
       borderHoverColor: "hover:border-green-500",
@@ -81,29 +89,46 @@ const Footer = () => {
               </h1>
             </div>
             <p className="sm:text-sm md:text-[1.120rem] lg:text-[1.2rem] lg:leading-6 ">
-              Wild Child Studios uses advanced AI to turn <br /> imagination into high-quality, creative visuals.
+              Wild Child Studios uses advanced AI to turn <br /> imagination
+              into high-quality, creative visuals.
             </p>
-            <div className="flex gap-6 ">
+
+            {/* Social Media Icons */}
+            <div className="flex gap-6">
               {socialLinks.map((social, index) => (
-                <Link
-                  key={index}
-                  href={social.href}
-                  className={`w-10 h-10 md:w-12 md:h-12 lg:w-12 lg:h-12 rounded-full flex items-center justify-center border-2 border-[#545454] bg-[#1E1E1E] transition-all duration-300 ${social.hoverColor} ${social.borderHoverColor} ${social.glowColor}`}
-                >
-                  <social.icon className="w-5 h-5 md:w-6 md:h-6 lg:w-6 lg:h-6 transition-colors duration-300" />
-                </Link>
+                <div key={index} className="relative group">
+                  {/* Social Icon */}
+                  <Link
+                    href={social.href}
+                    className={`w-10 h-10 md:w-12 md:h-12 md:mt-2 lg:w-12 lg:h-12 rounded-full flex items-center justify-center border-2 border-[#545454] bg-[#1E1E1E] 
+        transition-transform duration-200 ease-in-out transform-gpu will-change-transform hover:scale-125 hover:-translate-y-2 
+        ${social.hoverColor} ${social.borderHoverColor} ${social.glowColor}`}
+                  >
+                    <social.icon className="w-5 h-5 md:w-7 md:h-7 lg:w-6 lg:h-6 transition-transform duration-100 ease-in-out hover:scale-110" />
+                  </Link>
+
+                  {/* Social Title */}
+                  <span className="absolute left-1/2 -translate-x-1/2 -top-8 bg-[#1E1E1E] text-white text-xs md:text-xs lg:text-sm px-2 py-1 rounded-md opacity-0 transition-opacity duration-200 group-hover:opacity-100">
+                    {social.title}
+                  </span>
+                </div>
               ))}
             </div>
           </div>
 
           {/* Navigation Links */}
           {Object.entries(navigationLinks).map(([category, links]) => (
-            <div key={category} className="mt-2 mb-6 mr-10 lg:mr-16">
-              <h2 className="font-semibold text-white md:text-lg lg:text-xl mb-4">{category}</h2>
+            <div key={category} className="mt-4 mb-4 mr-10 lg:mr-16">
+              <h2 className="font-semibold text-white md:text-lg lg:text-xl mb-4">
+                {category}
+              </h2>
               <ul className="space-y-2">
                 {Object.entries(links).map(([name, href]) => (
                   <li key={name} className="pb-2 mb-2">
-                    <Link href={href} className="text-[#616161]  md:text-[0.9rem] hover:text-white transition-colors">
+                    <Link
+                      href={href}
+                      className="text-[#616161]  md:text-[0.9rem] hover:text-white transition-colors"
+                    >
                       {name}
                     </Link>
                   </li>
@@ -119,7 +144,7 @@ const Footer = () => {
             <p className="text-sm md:text-[0.875rem] lg:text-[1rem] text-[#616161] mb-4 md:mb-0">
               Copyright © 2025 WildMind Pvt ltd. All rights reserved.
             </p>
-            <div className="flex flex-wrap sm:gap-4 md:gap-[2.6rem] lg:gap-[4rem] justify-center md:ml-[31.4%] lg:ml-[37%]">
+            <div className="flex flex-wrap sm:gap-4 md:gap-[2.4rem] lg:gap-[4rem] justify-center md:ml-[31.4%] lg:ml-[37%]">
               {legalLinks.map((link) => (
                 <Link
                   key={link.name}
