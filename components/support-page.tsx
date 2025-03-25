88014a2021e8fa63beb834dfe24c31c7839aa9a8
Aryan Prajapati <prajapatiaryan3103.com>
2025-03-25 15:45:40 +0530

    message

--- components/support-page.tsx
+++ components/support-page.tsx
@@ -44,7 +44,7 @@ export default function SupportPage() {
   return (
 
 
-    <div className="min-h-screen bg-black text-white px-4 md:px-6 lg:px-8  md:py-12 lg:py-12">
+    <div className="min-h-screen bg-black text-white px-4 md:px-6 lg:px-8  pt-20">
       {/* Grid Container */}
       <div className="max-w-[90vw] mx-auto">
         {/* Top Row */}
