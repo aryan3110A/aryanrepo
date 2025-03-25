bd9fdd681b5aa1daacfa7f70585207f01e34ed59
Aryan Prajapati <prajapatiaryan3103.com>
2025-03-25 11:19:57 +0530

    message

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
         {/* <Footer /> */}
 
         {/* <Page /> */}
-        {/* <HomePage /> */}
+        <HomePage /> 
         {/* <ArtStation /> */}
         <TextGenerationTemplate />
       </body>
